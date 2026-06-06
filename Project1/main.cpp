6afcae3bbd3e27947c14158f2b30d90764f8fcee
User <User@DESKTOP-I5RC38D>
2026-06-06 23:06:26 +0200

    Add project files.

---
+++ Project1/main.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+
+using namespace std;
+
+void drawHangman(int mistakes)
+{
+    cout << "\n";
+
+    if (mistakes == 0)
+    {
+        cout << " +---+\n";
+        cout << " |   |\n";
+        cout << "     |\n";
+        cout << "     |\n";
+        cout << "     |\n";
+        cout << "     |\n";
+        cout << "=======\n";
+    }
+    else if (mistakes == 1)
+    {
+        cout << " +---+\n";
+        cout << " |   |\n";
+        cout << " O   |\n";
+        cout << "     |\n";
+        cout << "     |\n";
+        cout << "     |\n";
+        cout << "=======\n";
+    }
+    else if (mistakes == 2)
+    {
+        cout << " +---+\n";
+        cout << " |   |\n";
+        cout << " O   |\n";
+        cout << " |   |\n";
+        cout << "     |\n";
+        cout << "     |\n";
+        cout << "=======\n";
+    }
+    else if (mistakes == 3)
+    {
+        cout << " +---+\n";
+        cout << " |   |\n";
+        cout << " O   |\n";
+        cout << "/|   |\n";
+        cout << "     |\n";
+        cout << "     |\n";
+        cout << "=======\n";
+    }
+    else if (mistakes == 4)
+    {
+        cout << " +---+\n";
+        cout << " |   |\n";
+        cout << " O   |\n";
+        cout << "/|\\  |\n";
+        cout << "     |\n";
+        cout << "     |\n";
+        cout << "=======\n";
+    }
+    else if (mistakes == 5)
+    {
+        cout << " +---+\n";
+        cout << " |   |\n";
+        cout << " O   |\n";
+        cout << "/|\\  |\n";
+        cout << "/    |\n";
+        cout << "     |\n";
+        cout << "=======\n";
+    }
+    else
+    {
+        cout << " +---+\n";
+        cout << " |   |\n";
+        cout << " O   |\n";
+        cout << "/|\\  |\n";
+        cout << "/ \\  |\n";
+        cout << "     |\n";
+        cout << "=======\n";
+    }
+}
+
+int main()
+{
+    system("chcp 65001 > nul");
+
+    const char* words[] =
+    {
+        "kozak",
+        "borscht",
+        "viburnum",
+        "sun",
+        "willow",
+        "tryzub",
+        "bread",
+        "stork"
+    };
+
+    int wordCount = 8;
+
+    const char* secretWord = words[rand() % wordCount];
+
+    int len = strlen(secretWord);
+
+    char hidden[50];
+
+    for (int i = 0; i < len; i++)
+        hidden[i] = '_';
+
+    hidden[len] = '\0';
+
+    char used[100] = "";
+    int mistakes = 0;
+
+    cout << "================================\n";
+    cout << "        ГРА ШИБЕНИЦЯ\n";
+    cout << "================================\n";
+    cout << "Пиши англійсикі літри\n";
+
+    while (mistakes < 6)
+    {
+        drawHangman(mistakes);
+
+        cout << "\nСлово: ";
+        for (int i = 0; i < len; i++)
+            cout << hidden[i] << " ";
+
+        cout << "\n\nВикористані літери: " << used << endl;
+
+        char letter;
+        cout << "\nВведіть літеру: ";
+        cin >> letter;
+
+        bool found = false;
+
+        for (int i = 0; i < len; i++)
+        {
+            if (secretWord[i] == letter)
+            {
+                hidden[i] = letter;
+                found = true;
+            }
+        }
+
+        int size = strlen(used);
+        used[size] = letter;
+        used[size + 1] = ' ';
+        used[size + 2] = '\0';
+
+        if (!found)
+        {
+            mistakes++;
+            cout << "\nНеправильно!\n";
+        }
+        else
+        {
+            cout << "\nПравильно!\n";
+        }
+
+        if (strcmp(hidden, secretWord) == 0)
+        {
+            cout << "\n🎉 Вітаємо! Ви перемогли!\n";
+            cout << "Загадане слово: " << secretWord << endl;
+            return 0;
+        }
+    }
+
+    drawHangman(6);
+
+    cout << "\n💀 Ви програли!\n";
+    cout << "Загадане слово було: " << secretWord << endl;
+
+    return 0;
+}
